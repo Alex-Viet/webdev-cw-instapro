2f6d55232d4fde0c8e9ec9fbe246e108c33d40fb
Alex-Viet <alex.night.power@gmail.com>
2023-06-16 16:03:58 +0300

    Все запросы в Api подключены корректно

--- api.js
+++ api.js
@@ -26,7 +26,7 @@ export function getPosts({ token }) {
 
 // https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
 export function registerUser({ login, password, name, imageUrl }) {
-  return fetch(baseHost + "/api/user", {
+  return fetch(baseHost + "/user", {
     method: "POST",
     body: JSON.stringify({
       login,
@@ -43,7 +43,7 @@ export function registerUser({ login, password, name, imageUrl }) {
 }
 
 export function loginUser({ login, password }) {
-  return fetch(baseHost + "/api/user/login", {
+  return fetch(baseHost + "/user/login", {
     method: "POST",
     body: JSON.stringify({
       login,
@@ -62,7 +62,7 @@ export function uploadImage({ file }) {
   const data = new FormData();
   data.append("file", file);
 
-  return fetch(baseHost + "/api/upload/image", {
+  return fetch(baseHost + "/upload/image", {
     method: "POST",
     body: data,
   }).then((response) => {
