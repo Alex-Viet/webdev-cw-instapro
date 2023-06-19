e96d07c3198e5533e0267bf0e563a3c44e0a1fa7
Alex-Viet <alex.night.power@gmail.com>
2023-06-19 20:18:18 +0300

    Реализована кнопка "вверх"

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -50,6 +50,7 @@ export function renderPostsPageComponent({ appEl, userPosts }) {
                 <ul class="posts">
                   ${postsHtml}
                 </ul>
+                <div class="btn-up btn-up_hide"></div>
               </div>`
 
   appEl.innerHTML = appHtml;
@@ -126,4 +127,58 @@ export function renderPostsPageComponent({ appEl, userPosts }) {
         })
     })
   }
+
+  const buttonUp = {
+    el: document.querySelector('.btn-up'),
+    scrolling: false,
+    show() {
+      if (this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+        this.el.classList.remove('btn-up_hide');
+        this.el.classList.add('btn-up_hiding');
+        window.setTimeout(() => {
+          this.el.classList.remove('btn-up_hiding');
+        }, 300);
+      }
+    },
+    hide() {
+      if (!this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+        this.el.classList.add('btn-up_hiding');
+        window.setTimeout(() => {
+          this.el.classList.add('btn-up_hide');
+          this.el.classList.remove('btn-up_hiding');
+        }, 300);
+      }
+    },
+    addEventListener() {
+      window.addEventListener('scroll', () => {
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+
+        if (this.scrolling && scrollY > 0) {
+          return;
+        }
+        this.scrolling = false;
+
+        if (scrollY > 400) {
+          this.show();
+        } else {
+          this.hide();
+        }
+      });
+
+      const btnUp = document.querySelector('.btn-up');
+
+      btnUp.onclick = () => {
+        this.scrolling = true;
+        this.hide();
+
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+
+  buttonUp.addEventListener();
 }
